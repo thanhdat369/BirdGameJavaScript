de4041b530810ea44da4f26c0bc23935fdd51ce8
thanhdat369 <dat.lethanh369@gmail.com>
2023-11-26 00:47:00 +0700

    Separate to class file

---
+++ config.js
@@ -0,0 +1,22 @@
+// set config 
+const FPS = 60;
+const DEFAULT_GAP = 50;
+const GRAVITY = 1;
+
+const TIME_PER_FRAME = 1000 / FPS; //1000ms
+// 1unit = 1px per frame in 60 FPS -> 120FPS 1 unit = 0.5px 
+const PX_FRAME_UNIT = 60/FPS * 1;
+const SPEEDSLOW = 1 * PX_FRAME_UNIT;
+
+// load images
+const img_bird = "images/bird.png";
+const img_background = "images/bg.png";
+const img_foreground = "images/fg.png";
+const img_pipeUp = "images/pipe.png";
+const img_pipeDown = "images/pipe.png";
+
+
+const DEFAULT_COORD_PIPES = [
+    {x:500,y:250, gap: 30 },
+    {x:550,y:250}
+]
---
+++ game_play.js
@@ -0,0 +1,72 @@
+class GamePlayDependencies {
+    constructor() {
+
+    }
+    registerBackgroundObject(obj){
+        this.backgroundDrawObject = obj;
+    }
+
+    registerForegroudObject(obj){
+        this.foregroundDrawObject = obj;
+    }
+
+    registerBirdObject(obj){
+        this.birdObject = obj;
+    }
+
+    registerScore(obj) {
+        this.scoreObject = obj;
+    }
+
+    getBackgroundObject(){
+        return this.backgroundDrawObject; 
+    }
+
+    getForegroudObject(){
+        return this.foregroundDrawObject;
+    }
+
+    getBirdObject(){
+        return this.birdObject;
+    }
+
+    getScore(obj) {
+        return this.scoreObject;
+    }
+}
+
+const GameState  = {
+    Playing: 1,
+    Lose: 0
+}
+
+class GamePlay {
+    constructor(gamePlayDependency){
+        this.gamePlayDependency = gamePlayDependency;
+        this.__getFromDependencies();
+        this.GamePlay = GameState.Lose;
+    }
+
+    __getFromDependencies() {
+        this.birdObject = this.gamePlayDependency.getBirdObject();
+        this.backgroundDrawObject = this.gamePlayDependency.getBackgroundObject();
+        this.foregroundDrawObject = this.gamePlayDependency.getForegroudObject();
+        this.scoreObject = this.gamePlayDependency.getScore();
+    }
+
+    onClickHandle() {
+        this.birdObject.up(30);
+    }
+
+    handle() {
+        this.backgroundDrawObject.draw();
+        this.birdObject.draw();
+        this.scoreObject.draw();
+
+	    this.birdObject.down(GRAVITY * SPEEDSLOW);
+    }
+
+    play() {
+        this.handle();
+    }
+}
